12871e2aa3ac75c477615d4967bbc8dfd8614d2f
lucas3213 <ltn.lic17@uea.edu.br>
2020-08-08 20:31:22 +0000

    codigo-lucas

--- matriz_adjacencia.c
+++ matriz_adjacencia.c
@@ -1,38 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef int** MatrizAdjacencia;
-MatrizAdjacencia CriaGrafo(int vertices){
+
+typedef int** matriz_adjacencia;
+
+matriz_adjacencia cria_grafo(int vertices){
 	int i,j;
-	
-	MatrizAdjacencia M;
-	
-	M = malloc(vertices * sizeof(int*));
+	matriz_adjacencia m = malloc(vertices * sizeof(int*));
 	for(i = 0;i<vertices;i++){
-		M[i] = malloc(vertices * sizeof(int));
+		m[i] = malloc(vertices * sizeof(int));
 		for(j=0;j<vertices;j++){
-			M[i][j] = 0;
+			printf("[] = ");
+			scanf("%d",&m[i][j])
 		}
+		printf("\n");
+		mostra_matriz(i,vertices,m);
 	}	
-	return M;
-	
-	
+	return m;
 }
 
-void mostra_matriz(int vertices,MatrizAdjacencia m){
+void mostra_matriz(int linhas, int vertices,matriz_adjacencia m){
     int i,j;
-    for(i = 0;i<vertices;i++){
-		for(j=0;j<vertices;j++){
-			printf("%d", m[i][j] );
+    for(i = 0;i < linhas;i++){
+		for(j = 0;j < vertices;j++){
+			printf("%d", m[i][j]);
 		}
          printf("\n");
-	}	
-       
+	}	     
 }
 
 
+int verifica_ligacao(int continuar_processo, matriz_adjacencia m){
+	int valor_linha,valor_coluna = 0;
+	while(continuar_processo){
+		printf("verificacao de interligacao de valores \ninforme-os:\n");
+		printf("valor linha: \n");
+		scanf("%d",&valor_linha);
+		printf("valor coluna: \n");
+		scanf("%d",&valor_coluna);
+		printf("quer continuar com o processo de verificao?\n");
+		scanf("%d",&continuar_processo);
+		if(continuar_processo > 1 || continuar_processo < 0){
+			while(continuar_processo > 1 || continuar_processo < 0){
+				printf("erro ao continuar o processo de verificacao. \n");
+				printf("insira uma resposta valida. \n");
+				printf("quer continuar com o processo de verificao?\n");
+				scanf("%d",&continuar_processo);
+			}
+		}
+		return verifica_ligacao(continuar_processo, m)
+	}
+	return 0 
+}
+
+void confirma_ligacao(int valor_linha, int valor_coluna, matriz_adjacencia m){
+	if (m[valor_linha][valor_coluna])
+	{
+		printf("HÁ INTERLIGACAO");
+	}else{
+		printf("NÃO HÁ INTERLIGACAO");
+	}
+}
+
 int main(){
-	MatrizAdjacencia G;
+	matriz_adjacencia g;
 	int vertices = 4;
-	G = CriaGrafo(vertices);
-    mostra_matriz(vertices,G);
+	int f;
+	g = cria_grafo(vertices);
+    f=verifica_ligacao(G);
 }
